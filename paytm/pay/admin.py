535677b98aa12e35b93d4d84ae160a89e39d2e7d
Rahul Aggarwal <rahul.aggarwal@travclan.com>
2021-09-17 11:37:36 +0530

    Initial Commit

--- paytm/pay/admin.py
+++ paytm/pay/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 from .models import Wallet
 
 # Register your models here.
-admin.site.register(Wallet)
+class WalletAdmin(admin.ModelAdmin):
+    list_display = ['id', 'username', 'balance', 'money_added', 'money_sent']
+
+admin.site.register(Wallet, WalletAdmin)
